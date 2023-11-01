87d34d65e3ce83be75ebc24a648a9abe02574517
henriquebrnetto <henriquebrnetto02@gmail.com>
2023-11-01 12:44:24 -0300

    adiciona novas constantes

--- const.py
+++ const.py
@@ -1,1 +1,17 @@
 img_sizes = {'blackjack' : (100,84), 'slot_machine' : (80,80), 'roleta' : (100,100), 'horse_race' : (255,90), 'poker' : (290,150)}
+
+traducao = {
+    'blackjack' : 'Blackjack',
+    'roleta' : 'Roleta',
+    'slot_machine' : 'Caça Níquel',
+    'horse_race' : 'Corrida de Cavalo',
+    'poker' : 'Poker',
+}
+
+explicacoes = {
+    'blackjack' : 'teste1 teste3 teste4 teste5 teste6 teste1 teste1 teste1 teste1 teste1 teste1 teste1 teste1 XXX teste1 teste3 teste4 test vrjbvsjofeaofos e1 teste1 teste1 teste1 teste1 teste1 teste1 XXX',
+    'roleta' : 'teste1 teste3 teste4 teste5 teste6 teste1 teste1 teste1 teste1 teste1 teste1 teste1 teste1.',
+    'slot_machine' : 'teste1 teste3 teste4 teste5 teste6 teste1 teste1 teste1 teste1 teste1 teste1 teste1 teste1.',
+    'horse_race' : 'teste4 oioi',
+    'poker' : 'teste5',
+}
