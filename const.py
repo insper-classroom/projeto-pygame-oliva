1baf12b8699fdd239fff8ab83dcfe5e017d4fdbf
henriquebrnetto <henriquebrnetto02@gmail.com>
2023-11-03 19:08:24 -0300

    muda texto

--- const.py
+++ const.py
@@ -10,7 +10,7 @@ traducao = {
 
 explicacoes = {
     'blackjack' : 'Neste jogo, o objetivo é chegar o mais próximo de 21, sem passar de 21. Ganha quem conseguir a maior soma.\nO jogo é você contra o Dealer, e as apostas são de $100.',
-    'roleta' : 'Seu objetivo é acertar o número ou classificação em que a bolinha vai cair.\nVocê pode apostar em quantos valores quiser ($10 por aposta). Os valores que você ganha depende do acerto:\n-Cor : $20\n-Dúzia : $30\n-Número : $360',
+    'roleta' : 'Seu objetivo é acertar o número ou classificação em que a bolinha vai cair.\nVocê pode apostar em quantos valores quiser ($10 por aposta). Os valores que você ganha depende do acerto:\n-Cor: $20\n-Dúzia: $30\n-Número: $360',
     'slot_machine' : 'O objetivo é conseguir alinhar imagens iguais.',
     'horse_race' : 'Qual cavalo é o mais rápido?',
     'poker' : 'Este jogo será jogado contra o Dealer. Seu objetivo é conseguir uma mão melhor que a do oponente juntando as cartas da sua mão com outras 3 da mesa.',
